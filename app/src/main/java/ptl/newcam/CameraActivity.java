93df29caa8e5d2b6f4fe09fb4d57391cd27654eb
phamtanlong <lpham@magmic.com>
2017-12-20 22:00:27 +0700

    Setup basic

--- app/src/main/java/ptl/newcam/CameraActivity.java
+++ app/src/main/java/ptl/newcam/CameraActivity.java
@@ -1,52 +1,54 @@
 package ptl.newcam;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class CameraActivity extends AppCompatActivity {
+
+    public final String TAG = "CamaraActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setupFullScreen();
+        checkPermissions();
+    }
+
+    private void setupFullScreen () {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_camera);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_camera, menu);
-        return true;
-    }
+    private void checkPermissions () {
+        int camera = ContextCompat.checkSelfPermission(CameraActivity.this,
+                Manifest.permission.CAMERA);
+        int write = ContextCompat.checkSelfPermission(CameraActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (camera != PackageManager.PERMISSION_GRANTED || write != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(CameraActivity.this,
+                    new String[]{ Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE },
+                    1);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    public void onClickCapture (View view) {
+        Log.i(TAG, "onClickCapture");
     }
 }
